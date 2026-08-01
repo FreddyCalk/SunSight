c7af6f2bf14e9514b16dc8f57ca24b6c247f8161
Freddy Calk <freddycalk@gmail.com>
2026-08-01 13:21:54 -0600

    version bump to go with last build fix

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -36,7 +36,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: identity.name,
   slug: 'sunsight',
-  version: '1.0.0',
+  version: '1.0.1',
   runtimeVersion: {
     policy: 'fingerprint',
   },
